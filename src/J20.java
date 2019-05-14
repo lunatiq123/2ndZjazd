7712bf03b47ef4aaf459844e67272e2be0bcbae1
lunatiq123 <fisherking@o2.pl>
2019-05-14 22:12:57 +0200

    tablica 20 losowych liczb zrobiona

--- src/J20.java
+++ src/J20.java
@@ -1,41 +1,29 @@
 import java.util.Random;
 
+
 public class J20 {
 
     public static void main(String[] args) {
+        Random ran = new Random();
+        int liczba;
 
+        int[] tablica = new int[20];
+        System.out.println("20 losowych liczb od 1 do 20 (tablica) : ");
 
+        for (int i = 0; i < tablica.length; i++) {
 
-        Random r = new Random();
-        int liczba = r.nextInt();
+            liczba = ran.nextInt(19) + 1;
+            tablica[i] = i + liczba;
 
-
-
-        System.out.println(liczba);
-
-
-        /*
-        for (int i = 0; i < numbers.length; i++) {
-
-            int liczba = r.nextInt();
-
-            if (liczba > 0 && liczba <= 20) {
-                numbers[i] = liczba;
-            }
+            System.out.print(" " + tablica[i]);
         }
-        System.out.println(numbers);
-
-         */
 
     }
-
 }
 
-
-
-/*
-Ćwiczenie J20
- Napisz program, który utworzy tablicę 20 liczb całkowitych z przedziału
-1...10, a następnie wypisze na ekranie ile razy każda z liczb z tego
-przedziału powtarza się w tablicy
- */
+ /*
+ Ćwiczenie J20
+  Napisz program, który utworzy tablicę 20 liczb całkowitych z przedziału
+ 1...10, a następnie wypisze na ekranie ile razy każda z liczb z tego
+ przedziału powtarza się w tablicy
+  */
