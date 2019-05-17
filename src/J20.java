9b31abee4cc49e56df25dd27c02dbf989b89d178
lunatiq123 <fisherking@o2.pl>
2019-05-17 14:11:54 +0200

    sprawdzamy zawartość tablicy

--- src/J20.java
+++ src/J20.java
@@ -6,28 +6,36 @@ public class J20 {
     public static void main(String[] args) {
         Random ran = new Random();
         int ile = 0;
-        int liczba;
+
         int[] tablica = new int[20];
         System.out.println("20 losowych liczb od 1 do 10 :");
 
         for (int i = 0; i < tablica.length; i++) {
-            tablica[i] = (ran.nextInt(10)+1);
+            tablica[i] = (ran.nextInt(10) + 1);
             System.out.print(" " + tablica[i]);
-
-       }
-        System.out.println();
-        System.out.println(Arrays.toString(tablica));
-        for (int x:tablica
-             ) {
-                ile++;
-            }
-
-        System.out.println("Liczba " +  " występuje " + ile + " razy");
-
         }
 
+        System.out.println();
+        System.out.println(Arrays.toString(tablica));
+
+
+        for (int x : tablica) {
+
+            for (int i = 0; i < tablica.length; i++) {
+
+                if (x == tablica[i]) {
+
+                    ile++;
+                }
+
+
+            }
+
+            System.out.println("Liczba " + x + " występuje " + ile + " razy");
+        }
 
     }
+}
 
 
 
