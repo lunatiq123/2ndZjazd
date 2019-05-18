7461ee46625fe9627e2d850fc07b867cd3e27a98
lunatiq123 <fisherking@o2.pl>
2019-05-18 16:58:36 +0200

    kalkulator

---
+++ src/J25_2/Kalkulator.java
@@ -0,0 +1,34 @@
+package J25_2;
+
+public class Kalkulator {
+
+    public Kalkulator(){
+
+    }
+    public void suma(int a, int b) {
+        int suma = a + b;
+        System.out.println(suma);
+    }
+
+    public void roznica(int a, int b) {
+        int roznica = a - b;
+        System.out.println(roznica);
+    }
+
+    public void iloczyn(int a, int b) {
+        int iloczyn = a * b;
+        System.out.println(iloczyn);
+    }
+
+    public void iloraz(int a, int b) {
+        int iloraz = a / b;
+        System.out.println(iloraz);
+    }
+
+    //metody : suma, róznica , iloczyn i iloraz
+    // kazda metoda przyjmuje dwa parametry i zwraca wynik
+
+
+
+
+}
---
+++ src/J25_2/Run.java
@@ -0,0 +1,23 @@
+package J25_2;
+
+public class Run {
+
+    Kalkulator calc = new Kalkulator();
+
+
+    // stwórz obiekt klasy Kalkulator a następnie
+    //będziesz prosić użytkownika o podanie:
+    // podaj 1 liczbę, podaj znak działania, podaj 2 liczbę
+    //  - Następnie program powinien zaprezentować poprawny wynik.
+    // - Program powinien zapytać czy chcesz dokonać kolejnych obliczeń T/N
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+
+
+
+}
