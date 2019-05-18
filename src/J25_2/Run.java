9efffac311caa551ae43719394901ec5bb7638a7
lunatiq123 <fisherking@o2.pl>
2019-05-18 20:32:53 +0200

    kalkulator

--- src/J25_2/Run.java
+++ src/J25_2/Run.java
@@ -1,9 +1,8 @@
 package J25_2;
 
+import java.util.Scanner;
+
 public class Run {
-
-    Kalkulator calc = new Kalkulator();
-
 
     // stwórz obiekt klasy Kalkulator a następnie
     //będziesz prosić użytkownika o podanie:
@@ -13,11 +12,40 @@ public class Run {
 
     public static void main(String[] args) {
 
+        int a;
+        int b;
+        String znak;
+        Scanner sc = new Scanner(System.in);
+        Kalkulator calc = new Kalkulator();
 
+        while (true){
 
+            System.out.println("Podaj liczbę a");
+            a = sc.nextInt();
+            System.out.println("Podaj znak działania (+,-,*,/");
+            znak = sc.next();
+            System.out.println("Podaj liczbę b");
+            b = sc.nextInt();
+
+            if (znak == "+") {
+                calc.suma(a, b);
+            } else if (znak == "-") {
+                calc.roznica(a, b);
+            } else if (znak == "*") {
+                calc.iloczyn(a, b);
+            } else if (znak == "/") {
+                calc.iloraz(a, b);
+            }
+
+            System.out.println("Czy chcesz dokonać następnych obliczeń ? (T/N)?");
+            String dec = sc.nextLine().toUpperCase();
+            if (dec.equals("T")) {
+
+            }else if (dec.equals("N")) {
+                break;
+            }
+
+        }
 
     }
-
-
-
 }
